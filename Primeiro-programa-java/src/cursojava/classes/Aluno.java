9b223103521b713e4ac0e4b894cdf2af84600cf7
danie <danie@192.168.10.108>
2020-07-14 20:26:19 -0300

    Orientação a Objetos, Instanciação, atributos e referências

---
+++ Primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,30 @@
+package cursojava.classes;
+
+/*Está nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+	
+	/*Esses são os atributos do Aluno*/
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+	public Aluno() {/*Cria os dados na memória - sendo padrão do java*/
+		
+	}
+	
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+}
--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,35 +1,24 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
+import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
 
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		String nota1 = JOptionPane.showInputDialog("Informe a nota 1 ");
-		String nota2 = JOptionPane.showInputDialog("Informe a nota 2 ");
-		String nota3 = JOptionPane.showInputDialog("Informe a nota 3 ");
-		String nota4 = JOptionPane.showInputDialog("Informe a nota 4 ");
 		
-		double dNota1 = Double.parseDouble(nota1);
-		double dNota2 = Double.parseDouble(nota2);
-		double dNota3 = Double.parseDouble(nota3);
-		double dNota4 = Double.parseDouble(nota4);
+		/*New Aluno é uma instancia (Criação de Objeto)*/
+		/*aluno1 é uma referencia para o objeto aluno*/
+		Aluno aluno1 = new Aluno(); /*Aqui será o João*/
 		
-		double media = (dNota1 + dNota2 + dNota3 + dNota4) /4;
+		Aluno aluno2 = new Aluno(); /*Aqui será o Pedro*/
 		
-		/*Média para aprovação é 70*/
+		Aluno aluno3 = new Aluno(); /*Aqui será o Alex*/
 		
-		if(media >= 50) {
-			if(media >= 70) {
-				JOptionPane.showInternalMessageDialog(null, "Aluno está aprovado com média de: " + media);
-			}else {
-				JOptionPane.showInternalMessageDialog(null, "Aluno está de recuperação com média de: " + media);
-			}
-		}else {
-			JOptionPane.showInternalMessageDialog(null, "Aluno está reprovado com média de: " + media);
-		}
+		Aluno aluno4 = new Aluno("Maria");
+		
+		Aluno aluno5 = new Aluno("José", 50);
 		
 	}
 }
