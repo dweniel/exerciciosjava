78126b86f44bc43d75b2d499b0342f6fa1c99eb0
danie <daniel@192.168.10.108>
2020-07-14 14:36:50 -0300

    Execicio que calcula a quantidade de carros para cada pessoa e quantos carros sobram.

--- Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,18 +1,35 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	/* Main é um metodo auto executavel em Java */
 	public static void main(String[] args) {
 
-		/* Estrutura de Repetição For com Break */
-
-		for (int numero = 0; numero <= 10; numero++) {
-			if (numero == 3) {
-				System.out.println("Oba, encontrei o número 3");
-				System.out.println("Estou parando de executar");
-				break;
-				}
-			}
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas?");
+		
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+		
+		int divisao = (int)(carroNumero/pessoaNumero);
+		
+		double resto = carroNumero % pessoaNumero;
+		
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão?");
+		
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao);
+		}else {
+			System.out.println("Acesso negado!");
+		}
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o RESTO da divisão?");
+		
+		if(resposta == 0) {
+			JOptionPane.showMessageDialog(null, "O resto da divisão é " + resto);
+		}else {
+			System.out.println("Acesso Negado!");
 		}
 	}
+}
